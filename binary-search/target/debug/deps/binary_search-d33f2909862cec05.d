a2ce8f06761f75909587e93f20b906c4b720e9d7
Sean Chen <seanchen11235@gmail.com>
2025-05-23 14:03:02 -0500

    Add gitignore

--- binary-search/target/debug/deps/binary_search-d33f2909862cec05.d
+++
@@ -1,9 +0,0 @@
-/Users/seanchen/Projects/rust-data-structures/binary-search/target/debug/deps/libbinary_search-d33f2909862cec05.rmeta: src/lib.rs Cargo.toml
-
-/Users/seanchen/Projects/rust-data-structures/binary-search/target/debug/deps/binary_search-d33f2909862cec05.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- binary-search/target/debug/deps/binary_search-d49d326615b8f8ba.d
+++
@@ -1,5 +0,0 @@
-/Users/seanchen/Projects/rust-data-structures/binary-search/target/debug/deps/binary_search-d49d326615b8f8ba: src/lib.rs
-
-/Users/seanchen/Projects/rust-data-structures/binary-search/target/debug/deps/binary_search-d49d326615b8f8ba.d: src/lib.rs
-
-src/lib.rs:
